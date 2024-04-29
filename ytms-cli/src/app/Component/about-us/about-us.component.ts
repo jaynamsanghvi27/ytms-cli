9d1d8c2d9fc1d02ebe54c7f0df33c61b5e3e5ece
Prathmesh  Ranadive <prathmesh.ranadive@Yash.com>
2024-04-29 09:51:49 +0530

    Smooth Trainer Transision

--- ytms-cli/src/app/Component/about-us/about-us.component.ts
+++ ytms-cli/src/app/Component/about-us/about-us.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/Core/services/auth.service';
+import { JwtService } from 'src/app/Core/services/jwt.service';
 
 @Component({
   selector: 'app-about-us',
@@ -7,4 +9,14 @@ import { Component } from '@angular/core';
 })
 export class AboutUsComponent {
   sideNavStatus: boolean = false;
+  userRole:String='';
+  constructor(private authService:AuthService,private jwtService:JwtService){}
+
+  ngOnInit(): void 
+  {
+    const token = this.authService.getToken();
+    const role = this.jwtService.getRoleFromToken(token);
+    const email = this.jwtService.getUserNameFromToken(token);
+    this.userRole=role;
+  }
 }
