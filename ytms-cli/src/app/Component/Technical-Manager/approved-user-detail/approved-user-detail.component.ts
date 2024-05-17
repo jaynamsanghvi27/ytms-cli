7c75a6afee14d4d9369a4c41b58579d199ee98fa
aakash.sanap <g637y4i3jrbsophqvaoqztuz7g2lnpnufs4f52s3dhmloy4mnouq>
2024-05-17 09:17:50 +0530

    export to excel

--- ytms-cli/src/app/Component/Technical-Manager/approved-user-detail/approved-user-detail.component.ts
+++ ytms-cli/src/app/Component/Technical-Manager/approved-user-detail/approved-user-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 import { UsersService } from 'src/app/Core/services/users.service';
+import * as ExcelJS from 'exceljs';
+import * as FileSaver from 'file-saver';
 
 @Component({
   selector: 'app-approved-user-detail',
@@ -25,5 +26,44 @@ export class ApprovedUserDetailComponent {
         this.activeUserDetails[i]['roleType']="";
       }
     })
+  }
+
+  async exportToExcel(): Promise<void>{
+    console.log("Export To Excel  : ");
+    
+    const data:any[] = this.modifiedData(this.activeUserDetails);
+
+     const workbook = new ExcelJS.Workbook();
+     const worksheet = workbook.addWorksheet('My Sheet');
+ 
+     worksheet.addRow(['Employee Id', 'Employee Email', 'Employee Name', 'FeedBack'], 'n');
+ 
+     worksheet.columns = [
+       { header: 'Name', key: 'Name', width: 10 },
+       { header: 'Email ID', key: 'Email ID', width: 10 },
+       { header: 'Role', key: 'Role', width: 10 },
+       { header: 'Unit', key: 'Unit', width: 10 }
+     ];
+ 
+     data.forEach(item => {
+       worksheet.addRow(item);
+     });
+ 
+     worksheet.getRow(1).font = { bold: true, size: 12 };
+ 
+     const buffer = await workbook.xlsx.writeBuffer();
+     const blob = new Blob([buffer], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+     FileSaver.saveAs(blob, 'ActiveUserList.xlsx');
+  }
+
+  modifiedData(users: any[]):any[]{
+    return users.map(user => (
+      { 
+        'Name': user.fullName,
+       'Email ID': user.emailAdd,
+        'Role':user.userRole.roleTypes,
+        'Unit':user.unit?.name,
+      })
+      );
   }
 }
